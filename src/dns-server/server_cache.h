c6f15a682874ea1b86a0a3ce87c0c934ac0a214a
Adam-Teng <656081024@qq.com>
2022-07-08 00:42:20 +0800

    [fix] merge conflict

--- src/dns-server/server_cache.h
+++ src/dns-server/server_cache.h
@@ -1,39 +1,14 @@
-<<<<<<< HEAD
-/*
- * ============================================================================
- *
- *       Filename:  server-cache.h
- *
- *    Description:  init the database and cache
- *
- *        Created:  06/28/2022
- *
- *         Author:  yhteng
- *
- * ============================================================================
- */
-
-#ifndef DNS_SERVER_CACHE_H
-#define DNS_SERVER_CACHE_H
-
-#include "args.h"
-#include "cache.h"
-=======
 #ifndef DNS_SERVER_CACHE_H
 #define DNS_SERVER_CACHE_H
 
 #include "../lru-cache/cache.h"
 #include "../lru-cache/lru.h"
 #include "args.h"
->>>>>>> dev
 #include "tree.h"
 
 extern tree_t *db_tree;
 extern cache_t *db_cache;
-<<<<<<< HEAD
-=======
 extern trie db_lru_cache;
->>>>>>> dev
 
 void server_cache_init(parameter_t *para);
 
