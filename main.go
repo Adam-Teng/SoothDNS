b88a3ed758d975fa81a24908ba05c7ff6386d3c1
Adam-Teng <656081024@qq.com>
2022-07-10 20:48:17 +0800

    [feat] args parse

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"soothDNS/args"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	// Instantiate an ArgParser instance.
+	parser := args.NewParser()
+	parser.Helptext = "Usage: example..."
+	parser.Version = "1.2.3"
+
+	// Register a flag and a string-valued option.
+	parser.NewFlag("foo f")
+	parser.NewStringOption("bar b", "fallback")
+
+	// Parse the command line arguments.
+	parser.Parse()
+	fmt.Println(parser)
 }
